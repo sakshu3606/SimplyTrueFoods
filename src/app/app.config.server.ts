3206bfbe6f9ba930dfbde619e174c586c01b6a81
sakshu3606 <128841194+sakshu3606@users.noreply.github.com>
2025-08-15 21:07:15 +0530

    Update Angular packages

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,14 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
+import { provideServerRendering } from '@angular/ssr';
+import { provideRouter } from '@angular/router';
+
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
+    provideRouter(serverRoutes)
   ]
 };
 
